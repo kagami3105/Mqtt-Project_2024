a59b0e1d1fa58fbfa6ca09e444a5abaafc8c32ce
Kagami <kagami@example.com>
2024-12-06 13:20:25 -0400

    Sensor read value

--- Mqtt/src/main.cpp
+++ Mqtt/src/main.cpp
@@ -3,54 +3,53 @@
 #include <ArduinoMqttClient.h>
 #include <WiFiNINA.h>
 
-
-char ssid[] = "UNIFI_IDO1";    // your network SSID (name)
-char pass[] = "41Bidules!";    // your network password (use for WPA, or use as key for WEP)
+char ssid[] = "UNIFI_IDO1"; // your network SSID (name)
+char pass[] = "41Bidules!"; // your network password (use for WPA, or use as key for WEP)
 
 WiFiClient wifiClient;
 MqttClient mqttClient(wifiClient);
 void onMqttMessage(int);
 
-const char broker[]    = "192.168.10.122";
-int        port        = 1883;
+const char broker[] = "192.168.10.122";
+int port = 1883;
 
-const char topic_A1[]  = "amine/Touche_Sensor";
-const char topic_A2[]  = "amine/Distance";
-const char topic_A3[]  = "amine/ledState";
-const char topic_A4[]  = "amine/Sound_Sensor";
-
+const char topic_A1[] = "amine/Touche_Sensor";
+const char topic_A2[] = "amine/Distance";
+const char topic_A3[] = "amine/ledState";
+const char topic_A4[] = "amine/Sound_Sensor";
 
 const long interval = 1000;
 unsigned long previousMillis = 0;
-
 
 #define toucheSensorPin 8
 #define ultraSonicPin A6
 #define ledPin 6
 #define soundSensorPin A1
 
-
 String toucheSensor;
 String ledState;
 int distance;
 
-void setup() {
+void setup()
+{
 
-   Serial.begin(9600);
+  Serial.begin(9600);
 
   pinMode(toucheSensorPin, INPUT);
   pinMode(ledPin, OUTPUT);
 
-// ___________________________________________________ Connect to Newtork ____________________________________________
-  while (!Serial) {
+  // ___________________________________________________ Connect to Newtork ____________________________________________
+  while (!Serial)
+  {
     ; // wait for serial port to connect. Needed for native USB port only
   }
-  
+
   // attempt to connect to WiFi network:
   Serial.print("Attempting to connect to WPA SSID: ");
   Serial.println(ssid);
 
-  while (WiFi.begin(ssid, pass) != WL_CONNECTED) {
+  while (WiFi.begin(ssid, pass) != WL_CONNECTED)
+  {
     // failed, retry
     Serial.print(".");
     delay(5000);
@@ -65,11 +64,13 @@ void setup() {
   Serial.print("Attempting to connect to the MQTT broker: ");
   Serial.println(broker);
 
-  if (!mqttClient.connect(broker, port)) {
+  if (!mqttClient.connect(broker, port))
+  {
     Serial.print("MQTT connection failed! Error code = ");
     Serial.println(mqttClient.connectError());
 
-    while (1);
+    while (1)
+      ;
   }
 
   Serial.println("You're connected to the MQTT broker!");
@@ -77,57 +78,58 @@ void setup() {
   // ____________________________________________________________________________________________________________________
 }
 
-void loop() {
+void loop()
+{
 
   mqttClient.poll();
 
   unsigned long currentMillis = millis();
-  
-  if (currentMillis - previousMillis >= interval) {
 
-// _________________________ Touche Sensor ______________________________
+  if (currentMillis - previousMillis >= interval)
+  {
 
-    if(digitalRead(toucheSensorPin) == HIGH)
-        toucheSensor = "ON";
-    else 
-        toucheSensor = "OFF";
+    // _________________________ Touche Sensor ______________________________
+
+    (digitalRead(toucheSensorPin) == HIGH) ? toucheSensor = "ON" : toucheSensor = "OFF";
 
     mqttClient.beginMessage(topic_A1);
     mqttClient.print(toucheSensor);
     mqttClient.endMessage();
-// _________________________________________________________________________
+    // _________________________________________________________________________
 
-// _________________________ ultraSonic Sensor _____________________________
+    // _________________________ ultraSonic Sensor _____________________________
 
     mqttClient.beginMessage(topic_A2);
     int distance = analogRead(ultraSonicPin) * 520 / 1023;
     mqttClient.print(distance);
     mqttClient.endMessage();
-// ________________________________________________________________________
+    // ________________________________________________________________________
 
-// _______________________________ led ____________________________________
+    // _______________________________ led ____________________________________
     Serial.println(distance);
 
-    if(distance < 50) {
+    if (distance < 50)
+    {
       digitalWrite(ledPin, HIGH);
       ledState = "ON";
     }
-    else {
+    else
+    {
       digitalWrite(ledPin, LOW);
       ledState = "OFF";
     }
-    
+
     mqttClient.beginMessage(topic_A3);
     mqttClient.print(ledState);
     mqttClient.endMessage();
 
-// ________________________________________________________________________
+    // ________________________________________________________________________
 
-// _____________________________ Sound Sensor _____________________________
+    // _____________________________ Sound Sensor _____________________________
 
     int val;
-    val = analogRead(soundSensorPin);   //connect sensor to Analog 0
-    Serial.println(val);//print the sound value to serial
+    val = analogRead(soundSensorPin); // connect sensor to Analog 0
+    Serial.println(val);              // print the sound value to serial
 
     mqttClient.beginMessage(topic_A4);
     mqttClient.print(val);
@@ -135,9 +137,6 @@ void loop() {
 
     // save the last time a message was sent
     previousMillis = currentMillis;
-
   }
-// ________________________________________________________________________
-
+  // ________________________________________________________________________
 }
-
